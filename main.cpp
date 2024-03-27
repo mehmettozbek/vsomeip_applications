7b781ec4b202c22097836a7994449db32644d665
Mehmet Özbek <90189279+mehmettozbek@users.noreply.github.com>
2024-03-27 15:03:14 +0100

    vsomeip app debug 3

--- main.cpp
+++ main.cpp
@@ -255,7 +255,7 @@ void my_message_handler(const std::shared_ptr<vsomeip_v3::message>& message) {
     //  std::cout << "HANDLER:  message_handler(" << *message << ")" << std::endl;
 
     std::cout << "----id=" << message.get_message() << "----" << std::endl;
-    std::cout << "----service=" << get_service_name(message.get_service()) << "----" << std::endl;
+    /*std::cout << "----service=" << get_service_name(message.get_service()) << "----" << std::endl;
     std::cout << "----instance=" << message.get_instance() << "/" << hex(message.get_instance()) << "----" << std::endl;
     std::cout << "----method=" << get_method_name(message.get_service(), message.get_method()) << "----" << std::endl;
     std::cout << "----length=" << message.get_length() << "----" << std::endl;
@@ -273,9 +273,9 @@ void my_message_handler(const std::shared_ptr<vsomeip_v3::message>& message) {
     std::cout << "----uid=" << message.get_uid() << "----" << std::endl;
     std::cout << "----gid=" << message.get_gid() << "----" << std::endl;
     std::cout << "----env=" << message.get_env() << "----" << std::endl;
-    std::cout << "----sec_client=" << message.get_sec_client() << "----" << std::endl;
+    std::cout << "----sec_client=" << message.get_sec_client() << "----" << std::endl;*/
 
-    if (message->get_method_name(message.get_service(), message.get_method()) == MESSAGE_ID_FILTER)
+    if (get_method_name(get_service_name(message.get_service()), message.get_method()) == MESSAGE_ID_FILTER)
          std::cout << "HANDLER:  message_handler(" << *message << ")" << std::endl;
 }
 
